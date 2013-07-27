7f399eb1e2e580fa55e609d4c02c629c009b415f
Lindsey Kuper <lindsey@composition.al>
2013-07-27 13:33:02 -0700

    A few more proofs from Induction.v.

--- Induction.v
+++ Induction.v
@@ -229,24 +229,44 @@ Proof.
 Theorem mult_0_r : forall n:nat,
   n * 0 = 0.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n. induction n as [| n'].
+  Case "n = 0".
+  reflexivity.
+  Case "n = S n'".
+  simpl. rewrite -> IHn'. reflexivity.
+Qed.
 
 Theorem plus_n_Sm : forall n m : nat, 
   S (n + m) = n + (S m).
 Proof. 
-  (* FILL IN HERE *) Admitted.
-
+  intros n m. induction n as [| n'].
+  Case "n = 0".
+  simpl. reflexivity.
+  Case "n = S n'".
+  simpl. rewrite -> IHn'. reflexivity.
+Qed.
 
 Theorem plus_comm : forall n m : nat,
   n + m = m + n.
 Proof.
-  (* FILL IN HERE *) Admitted.
-
+  intros m n. induction n as [| n'].
+  Case "n = 0".
+  simpl. rewrite <- plus_0_r. reflexivity.
+  Case "n = S n'".
+  simpl. rewrite <- plus_n_Sm.
+  rewrite <- IHn'. reflexivity.
+Qed.
 
 Theorem plus_assoc : forall n m p : nat,
   n + (m + p) = (n + m) + p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros n m p.
+  induction n as [| n'].
+  Case "n = 0".
+  simpl. reflexivity.
+  Case "n = S n'".
+  simpl. rewrite <- IHn'. reflexivity.
+Qed.
 (** [] *)
 
 (** **** Exercise: 2 stars (double_plus) *)
